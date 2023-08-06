dd11798e2a1b06e23f6c2166efc56f41bbd081e8
Lucas Pereira <lucas.lmspereira@gmail.com>
2023-08-06 11:03:00 -0300

    remove plot example from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,8 @@
 #include "..\knn\knn.h"
 #include "..\kmeans\kmeans.h"
 
-#include <matplot/matplot.h>
 
 int main() {
-
-	using namespace matplot;
-	std::vector<double> t = iota(0, pi / 50, 10 * pi);
-	std::vector<double> st = transform(t, [](auto x) { return sin(x); });
-	std::vector<double> ct = transform(t, [](auto x) { return cos(x); });
-	auto l = plot3(st, ct, t);
-	show();
 
 	data_handler* dh = new data_handler();
 	// Read images from dataset.
@@ -25,7 +17,7 @@ int main() {
 	dh->count_classes();
 
 	// Follow procedures for kmeans
-	kmeansProcedures(dh);
+	// kmeansProcedures(dh);
 
 	// Follow procedures for knn
 	knnProcedures(dh);
